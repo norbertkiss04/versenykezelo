581abd3b165fe87645106c0477af8c3dc8a132e8
Norbi <kissn309@gmail.com>
2024-09-12 08:07:43 +0200

    css adjustments

--- resources/views/components/competition-cards.blade.php
+++ resources/views/components/competition-cards.blade.php
@@ -5,28 +5,26 @@
         </div>
         <div class="card-body p-4">
             <div class="row align-items-center">
-                <div class="col-md-8">
-                    <p class="card-text mb-2">
-                        <strong>Év:</strong>
-                        <span class="text-muted">{{ $competition->year }}</span>
-                    </p>
-                    <p class="card-text mb-2">
-                        <strong>Nyermény:</strong>
-                        <span class="text-success">${{ number_format($competition->prize_pool, 2) }}</span>
-                    </p>
-                    <p class="card-text mb-2">
-                        <strong>Kezdődik:</strong>
-                        <span class="text-muted">{{ \Carbon\Carbon::parse($competition->start_date)->format('Y, M d') }}</span>
-                    </p>
-                    <p class="card-text mb-2">
-                        <strong>Végződik:</strong>
-                        <span class="text-muted">{{ \Carbon\Carbon::parse($competition->end_date)->format('Y, M d') }}</span>
-                    </p>
-                </div>
+                <p class="card-text mb-2">
+                    <strong>Év:</strong>
+                    <span class="text-muted">{{ $competition->year }}</span>
+                </p>
+                <p class="card-text mb-2">
+                    <strong>Nyermény:</strong>
+                    <span class="text-success">${{ number_format($competition->prize_pool, 2) }}</span>
+                </p>
+                <p class="card-text mb-2">
+                    <strong>Kezdődik:</strong>
+                    <span class="text-muted">{{ \Carbon\Carbon::parse($competition->start_date)->format('Y, M d') }}</span>
+                </p>
+                <p class="card-text mb-2">
+                    <strong>Végződik:</strong>
+                    <span class="text-muted">{{ \Carbon\Carbon::parse($competition->end_date)->format('Y, M d') }}</span>
+                </p>
             </div>
 
             <div class="mt-4">
-                <h6>Forduló:</h6>
+                <h6>Fordulók:</h6>
                 <ul class="list-group mb-3">
                     @foreach($competition->round as $round)
                         <li class="list-group-item cursor-pointer round" data-round-id="{{ $round->id }}">
--- resources/views/components/competitor-modal.blade.php
+++ resources/views/components/competitor-modal.blade.php
@@ -9,13 +9,15 @@
                 <ul id="competitorsList" class="list-group mb-3">
                 </ul>
 
-                <select id="availableUsersDropdown" class="form-select mb-3" data-admin-only="true" style="display: none">
-                    <option value="">Felhasználó</option>
-                </select>
-                <button id="addCompetitorButton" class="btn btn-primary" data-admin-only="true" style="display: none">Versenyző hozzáadása</button>
-                <button id="deleteRoundButton" class="btn btn-danger mt-3" data-admin-only="true" style="display: none">
-                    Forduló törlése
-                </button>
+                    <select id="availableUsersDropdown" class="form-select mb-3" data-admin-only="true" style="display: none">
+                        <option value="">Felhasználó</option>
+                    </select>
+                <div class="d-flex align-items-center justify-content-between">
+                    <button id="addCompetitorButton" class="btn btn-primary" data-admin-only="true" style="display: none">Versenyző hozzáadása</button>
+                    <button id="deleteRoundButton" class="btn btn-danger" data-admin-only="true" style="display: none">
+                        Forduló törlése
+                    </button>
+                </div>
             </div>
         </div>
     </div>
